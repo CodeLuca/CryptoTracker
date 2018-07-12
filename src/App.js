f18031c48d7c0b558b09c2450807c0322117c6fe
Luca Marchal <lucamarchal@gmail.com>
2018-07-12 10:21:33 +0100

    initialise repo, add router

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import Home from './Home';
+export default () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+    </div>
+  </Router>
+)
